fbbe77b845944a6065d43148783456a6ec93c2cf
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-03-20 22:44:41 +0100

    adding post request to coffees controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
 
-    @Get()
+    @Get('flavors')
     findAll(){
         return "this return all coffees";
     }
 
+    @Get(':id')
+    findOne(@Param('id') id: string){
+        return `this return this #${id} coffee`;
+    }
+
+    @Post()
+    create (@Body('lastname') body){
+        console.log(body)
+        return body;
+        // return `This action creates a coffee`;
+    }
 }
