67b3f9768d2a7c7a56c6e8972bdd8620fecbe69f
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-03-21 11:46:14 +0100

    adding patch and delete methods

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
 
     @Get('flavors')
-    findAll(@Res() response){
-        response.status(200).send("this return all coffees");
+    findAll(){
+        return 'this return all coffees';
     }
 
     @Get(':id')
@@ -19,5 +19,16 @@ export class CoffeesController {
         console.log(body)
         return body;
         // return `This action creates a coffee`;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body){
+        console.log(body);
+        return `this action update #${id} coffee`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id : string) {
+        return `this action removes #${id} coffee`;
     }
 }
