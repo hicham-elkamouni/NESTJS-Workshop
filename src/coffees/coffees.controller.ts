2b90bd1cafa325c75df5a15487c7eca407fd7af8
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2022-03-26 19:37:37 +0100

    adding DTO

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -22,14 +24,13 @@ export class CoffeesController {
     }
 
     @Post()
-    // @HttpCode(HttpStatus.GONE)
-    create (@Body() body){
-        return this.coffeesService.create(body);
+    create (@Body() createCoffeeDto: CreateCoffeeDto){
+        return this.coffeesService.create(createCoffeeDto);
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() body){
-        return this.coffeesService.update(id, body)
+    update(@Param('id') id: string, @Body() updateCoffeeDto : UpdateCoffeeDto){
+        return this.coffeesService.update(id, updateCoffeeDto);
     }
 
     @Delete(':id')
